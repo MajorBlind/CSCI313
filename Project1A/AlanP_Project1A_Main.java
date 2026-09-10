ba67f2fff76792a4080cb2e06609ec628fe9b462
MajorBlind <alan.opac16@gmail.com>
2026-09-10 17:57:27 -0400

    Create output and logfile files & Add sort class in main file

--- Project1A/AlanP_Project1A_Main.java
+++ Project1A/AlanP_Project1A_Main.java
@@ -3,13 +3,32 @@ package Project1A;
 import java.io.*;
 import java.util.*;
 
+public class sort{
+    private int count;
+    private String[] dataAry;
+
+    //Constructor
+    public sort(int count, String[] dataAry){
+        this.count = count;
+        this.dataAry = dataAry;
+    }
+    //int countData - read/return count of data in inFile
+
+    //loadData - Read data from inFile and store in dataAry
+
+    //print dataAry(dataAry, fileOut) - output each data in dataAry to fileOut, 0 -> -1
+    //one data per text line
+}
+
 public class AlanP_Project1A_Main{
     public static void main(String[] args)throws IOException{
+        //Check args amount is correct
         if(args.length != 2){
             System.out.println("Program needs 2 arguments.");
             System.exit(1);
         }
 
+        //Init file reader/writers and check to see if they open
         Scanner inFile = null;
         PrintWriter outFile = null;
         PrintWriter logFile = null;
