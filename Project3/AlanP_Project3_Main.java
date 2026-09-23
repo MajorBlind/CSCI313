c84d09794cafd6b6347062fe52ee3d0869cb6678
MajorBlind <alan.opac16@gmail.com>
2026-09-23 17:17:26 -0400

    Create new logfile and output for 2nd dataset rename first output and logfile for the first dataset

--- Project3/AlanP_Project3_Main.java
+++ Project3/AlanP_Project3_Main.java
@@ -1,3 +1,10 @@
+import java.io.*; // For file I/O
+import java.util.*; // For console I/O and other built-in funcs
+
+class LLStack {
+    LLStack(){}
+}
+
 public class AlanP_Project3_Main {
     
 }
